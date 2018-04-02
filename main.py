b1a76c624beced9d30b6baa84c6666b85b7340e2
Christoph <chris24.blessing@gmail.com>
2018-04-02 02:42:40 +0200

    First working prototype

--- main.py
+++ main.py
@@ -1,35 +1,55 @@
-import time
+import random
 
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 
 
-driver = webdriver.Chrome()
-driver.get('https://stadtlandflussonline.net/g/AZYICJFQ')
-rounds = int(driver.find_element_by_css_selector('.alert.alert-info').text[-1])
-driver.find_element_by_id('gameForm:joinMe').click()
-driver.close()
+def main():
+    driver = webdriver.Chrome()
+    driver.get('https://stadtlandflussonline.net/g/AP7ICPNE')
+    rounds = int(driver.find_element_by_css_selector('.alert.alert-info').text[-1])
+    join_button = driver.find_element_by_id('gameForm:joinMe').find_element_by_tag_name('a')
+    join_button.send_keys(Keys.RETURN)
+    for round in range(rounds):
+        WebDriverWait(driver, 60).until(EC.title_contains('WRITING_CATEGORIES'))
+        current_letter = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, 'currentLetter'))).text
+        round_inputs = driver.find_elements_by_class_name('form-group')[:-1]
+        for round_input in round_inputs:
+            category = round_input.text
+            input_field = round_input.find_element_by_tag_name('input')
+            input_field.clear()
+            input_field.send_keys(get_answer(category, current_letter))
+        submit_button = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, 'gameForm:checkSendBtn')))
+        submit_button.send_keys(Keys.RETURN)
+        results_button = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, 'gameForm:j_idt226')))
+        results_button.send_keys(Keys.RETURN)
 
 
+def get_answer(category, current_letter):
+    base_url = 'https://www.stadt-land-fluss-online.de'
+    links = {
+        'Stadt': 'Städte',
+        'Land': 'Länder',
+        'Fluss': 'Flüsse',
+        'Vorname': 'Namen',
+        'Tier': 'Tiere',
+        'Beruf': 'Berufe',
+        'Pflanze': 'Pflanzen',
+        'Band/Musiker': 'Musiker',
+        'Filme/Serien': 'Filmtitel'
+    }
+    url = f'{base_url}/buchstabe-{current_letter.lower()}'
+    res = requests.get(url, verify=False)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, 'html.parser')
+    answer_tags = soup.find('h3', text=f'{links[category]} mit {current_letter.upper()}').next_sibling.find_all('li')
+    return random.choice(answer_tags).text
 
-'''
-driver.find_element_by_id('gameForm:joinMe').click()
-WebDriverWait(driver, 120).until(EC.title_contains('WRITING_CATEGORIES'))
-rounds = int(driver.title.split('/')[-1][0])
-for round in range(rounds):
-    current_letter = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.ID, 'currentLetter'))).text
-    round_inputs = driver.find_elements_by_class_name('form-group')[:-1]
-    for round_input in round_inputs:
-        category = round_input.text
-        input_field = round_input.find_element_by_tag_name('input')
-        input_field.clear()
-        input_field.send_keys(current_letter + category)
-    driver.find_element_by_id('gameForm:checkSendBtn').click()
-    confirm_results = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.ID, 'gameForm:j_idt226')))
-    confirm_results.click()
-    WebDriverWait(driver, 120).until(EC.title_contains('WRITING_CATEGORIES'))
-driver.close()
-'''
+
+if __name__ == '__main__':
+    main()
