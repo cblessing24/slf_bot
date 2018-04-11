f270a08b360d157f3da7de5226333436966e0600
christoph <chris24.blessing@gmail.com>
2018-04-11 12:28:08 +0200

    SLFDatabase scraping answers

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ class SLFBot:
 
     game_base_url = 'https://stadtlandflussonline.net'
 
-    def __init__(self, database_path='slf_database', wait=120):
-        self.database = SLFDatabase(database_path)
+    def __init__(self, database, wait=120):
+        self.database = database
         self.wait = wait
         self.driver = webdriver.Chrome()
 
@@ -163,39 +163,38 @@ class SLFDatabase:
         for category in self.database.keys():
             print(category)
 
+    def scrape_answers(self):
+        couplers = {
+            'Städte': 'Stadt',
+            'Länder': 'Land',
+            'Flüsse': 'Fluss'
+        }
+        url = 'https://www.stadt-land-fluss-online.de/kategorien/'
+        res = requests.get(url, verify=False)
+        res.raise_for_status()
+        soup = BeautifulSoup(res.text, 'html.parser')
+        category_tags = [tag.find('a') for tag in soup.find_all('h3', limit=3)]
+        categories = [tag.text.split()[0] for tag in category_tags]
+        category_links = [tag['href'] for tag in category_tags]
+        for category, category_link in zip(categories, category_links):
+            res_category = requests.get(category_link, verify=False)
+            res_category.raise_for_status()
+            soup_category = BeautifulSoup(res_category.text, 'html.parser')
+            list_tags = soup_category.find('div', class_='post-content').find('ul').find_all('li')
+            letters = [tag.text.split(':')[0][-1] for tag in list_tags]
+            answers = [tag.text.split(':')[1].strip() for tag in list_tags]
+            for index, answer in reversed(list(enumerate(answers))):
+                if answer.startswith('Es gibt'):
+                    del letters[index]
+                    del answers[index]
+            for letter, answer in zip(letters, answers):
+                self.add_answer(couplers[category], letter, answer)
+
 
 def main():
     slf_database = SLFDatabase('slf_database')
-    slf_database.reset()
-    print(slf_database.get_answers('Stadt', 'B'))
-
-
-def download_answers(database):
-    couplers = {
-        'Städte': 'Stadt',
-        'Länder': 'Land',
-        'Flüsse': 'Fluss'
-    }
-    url = 'https://www.stadt-land-fluss-online.de/kategorien/'
-    res = requests.get(url, verify=False)
-    res.raise_for_status()
-    soup = BeautifulSoup(res.text, 'html.parser')
-    category_tags = [tag.find('a') for tag in soup.find_all('h3', limit=3)]
-    categories = [tag.text.split()[0] for tag in category_tags]
-    category_links = [tag['href'] for tag in category_tags]
-    for category, category_link in zip(categories, category_links):
-        res_category = requests.get(category_link, verify=False)
-        res_category.raise_for_status()
-        soup_category = BeautifulSoup(res_category.text, 'html.parser')
-        list_tags = soup_category.find('div', class_='post-content').find('ul').find_all('li')
-        letters = [tag.text.split(':')[0][-1] for tag in list_tags]
-        answers = [tag.text.split(':')[1].strip() for tag in list_tags]
-        for index, answer in reversed(list(enumerate(answers))):
-            if answer.startswith('Es gibt'):
-                del letters[index]
-                del answers[index]
-        for letter, answer in zip(letters, answers):
-            database.add_answer(couplers[category], letter, answer)
+    slf_bot = SLFBot(slf_database)
+    slf_bot.play('https://stadtlandflussonline.net/g/AO2IFBWI')
 
 
 if __name__ == '__main__':
